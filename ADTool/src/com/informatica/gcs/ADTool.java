49b6f87e0eafa65ee519cd3f744f80d437511f42
wchung <wchung@CAW177332.informatica.com>
2014-05-05 14:53:30 -0700

    cm

--- ADTool/src/com/informatica/gcs/ADTool.java
+++ ADTool/src/com/informatica/gcs/ADTool.java
@@ -5,6 +5,10 @@ public class ADTool {
 	String inputDelimiter =";"; // default input delimeter
 	
 	public ADTool () {
-		
+		//Initialize AddressDoctor input file format
+		//	check default file location
+		//Initialize Java environment
+		//	check location of Java
+		//Initialize AD environment
 	}
 }
