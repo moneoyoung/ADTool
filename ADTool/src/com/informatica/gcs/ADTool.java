735dabc07d60aff9dcfd6623c2fa626967a45b97
wchung <wchung@CAW177332.informatica.com>
2014-05-05 16:22:02 -0700

    output

--- ADTool/src/com/informatica/gcs/ADTool.java
+++ ADTool/src/com/informatica/gcs/ADTool.java
@@ -1,14 +1,31 @@
 package com.informatica.gcs;
 
+import java.lang.String;
+import java.util.List;
+import java.io.File;
+
 public class ADTool {
 	String srcFileName = ""; //default source file for AD input
-	String inputDelimiter =";"; // default input delimeter
+	String inputDelimiter =";"; // default input delimiter for input data file
+	String addressInputFormatFile = "InputConfig.xml";
 	
 	public ADTool () {
-		//Initialize AddressDoctor input file format
+		//Initialize AddressDoctor input format file
+		
 		//	check default file location
 		//Initialize Java environment
 		//	check location of Java
 		//Initialize AD environment
+	}
+	
+	List<String> readAddressInputFormatFile() {
+		 
+		try {
+			File configXml = new File(addressInputFormatFile);
+		} catch (Exception e) {
+			
+		}
+		
+		return "";
 	}
 }
