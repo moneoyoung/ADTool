b867adc392c37c4f9cd1a7a6fd0b1df30c9c91c5
wchung <wchung@CAW177332.informatica.com>
2014-05-05 14:42:49 -0700

    2nd commit

--- ADTool/src/com/informatica/gcs/ADTool.java
+++ ADTool/src/com/informatica/gcs/ADTool.java
@@ -1,9 +1,9 @@
 package com.informatica.gcs;
 
 public class ADTool {
-	String srcFileName = "";
+	String srcFileName = ""; //default source file for AD input
+	String inputDelimiter =";"; // default input delimeter
 	
-
 	public ADTool () {
 		
 	}
