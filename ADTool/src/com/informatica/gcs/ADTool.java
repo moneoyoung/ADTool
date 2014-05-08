798562067acf0690f30eadac7adea53f36e9fd3b
wchung <wchung@CAW177332.informatica.com>
2014-05-08 16:22:31 -0700

    finshed readAddressInputFormatFile

--- ADTool/src/com/informatica/gcs/ADTool.java
+++ ADTool/src/com/informatica/gcs/ADTool.java
@@ -2,30 +2,47 @@ package com.informatica.gcs;
 
 import java.lang.String;
 import java.util.List;
-import java.io.File;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class ADTool {
 	String srcFileName = ""; //default source file for AD input
 	String inputDelimiter =";"; // default input delimiter for input data file
-	String addressInputFormatFile = "InputConfig.xml";
+	static String addressInputFormatFile = "InputFormat.cfg";
 	
 	public ADTool () {
 		//Initialize AddressDoctor input format file
-		
+		readAddressInputFormatFile();
 		//	check default file location
 		//Initialize Java environment
 		//	check location of Java
 		//Initialize AD environment
 	}
 	
-	List<String> readAddressInputFormatFile() {
-		 
+	static List<String> readAddressInputFormatFile() {
+		String currentLine;
+		BufferedReader br = null;
+		List<String> addressElement = new ArrayList<String>();
+	 
 		try {
-			File configXml = new File(addressInputFormatFile);
-		} catch (Exception e) {
+			FileReader inputConfigFile = new FileReader(addressInputFormatFile);
+			br = new BufferedReader(inputConfigFile);
 			
+			while ((currentLine = br.readLine()) != null) {
+				addressElement.add(currentLine);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
 		}
 		
-		return "";
+		return addressElement;
 	}
 }
