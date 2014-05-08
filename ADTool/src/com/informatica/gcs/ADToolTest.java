1a0188a9a5cace0433387b8a7af736b39235cf1c
wchung <wchung@CAW177332.informatica.com>
2014-05-08 16:33:24 -0700

    test

---
+++ ADTool/src/com/informatica/gcs/ADToolTest.java
@@ -0,0 +1,11 @@
+package com.informatica.gcs;
+
+public class ADToolTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ADTool at = new ADTool(); 
+		at.printAddressElement();
+	}
+
+}
